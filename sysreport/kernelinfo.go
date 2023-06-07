abb8a7cd171e47888051e57e7f2069d4b419ef1a
Fred Young <fred@wanion.net>
2023-06-07 22:05:14 +1200

    revert accidental change to kernelinfo.go

--- sysreport/kernelinfo.go
+++ sysreport/kernelinfo.go
@@ -1,18 +1,23 @@
 package sysreport
 
+import (
+	"log"
+	"syscall"
+)
+
 // getKernelInfo gets information with the `uname` syscall instead of running the `uname`
 // utility repeatedly.
 func getKernelInfo() (uname struct{ name, release, version, architecture string }) {
-	// var un syscall.Utsname
-	// err := syscall.Uname(&un)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+	var un syscall.Utsname
+	err := syscall.Uname(&un)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	// uname.architecture = convertFromCString(un.Machine)
-	// uname.release = convertFromCString(un.Release)
-	// uname.version = convertFromCString(un.Version)
-	// uname.name = convertFromCString(un.Sysname)
+	uname.architecture = convertFromCString(un.Machine)
+	uname.release = convertFromCString(un.Release)
+	uname.version = convertFromCString(un.Version)
+	uname.name = convertFromCString(un.Sysname)
 
 	return uname
 }
